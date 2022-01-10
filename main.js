0a8aad19392d229026b26f941c4a51834660ea20
Alessio-Tomei <alessiotomei22@gmail.com>
2022-01-10 16:01:43 +0100

    Consegna Finita

--- main.js
+++ main.js
@@ -30,3 +30,42 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+for (let i = 0; i < team.length; i++) {
+  generateTeamCard(i); 
+}
+
+const addMemberButton = document.getElementById('addMemberButton');
+
+addMemberButton.addEventListener('click', newTeamMember);
+
+function newTeamMember() {
+  const newName = document.getElementById('name').value;
+  const newRole = document.getElementById('role').value;
+  const newImage = document.getElementById('image').value;
+  const newMember = {
+    name : newName,
+    role : newRole,
+    image : newImage
+  };
+  team.push(newMember);
+  generateTeamCard(team.length-1);
+  document.getElementById('name').value ='';
+  document.getElementById('role').value = '';
+  document.getElementById('image').value = '';
+}
+
+function generateTeamCard(index) {
+  const teamContainer = document.querySelector('.team-container');
+  let element = `
+  <div class="team-card">
+    <div class="card-image">
+      <img src="img/${team[index].image}" alt="${team[index].name}">
+  </div>
+  <div class="card-text">
+      <h3>${team[index].name}</h3>
+      <p>${team[index].role}</p>
+    </div>
+  </div>`;
+  teamContainer.insertAdjacentHTML('beforeend', element);
+}
